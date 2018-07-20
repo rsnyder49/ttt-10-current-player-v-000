8e1ae18bf163513ec3d6bd8b6cb3e8a2ab2f185b
Robert Snyder <r.snyder49@gmail.com>
2018-07-20 22:24:20 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,18 @@
+def turn_count(board)
+  counter = 0 
+  board.each do |turn|
+    if turn ===  "X" || turn === "O"
+      counter += 1 
+      end
+  end
+return counter
+end
+
+def current_player(board)
+  whos_turn = turn_count(board)
+  if whos_turn % 2 === 0 
+    return "X"
+  else
+    return "O"
+  end
+end
